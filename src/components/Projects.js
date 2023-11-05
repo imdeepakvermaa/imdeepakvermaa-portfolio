13c7d0cc12edf03b02c7097ae8423665f1b3c82a
imdeepakvermaa <dv24012002@gmail.com>
2023-11-06 00:27:37 +0530

    Almost frontend part done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,11 @@
+import img1 from "../images/duoo.png";
+import img2 from "../images/netflix.PNG";
+import img3 from "../images/foodorder.PNG";
+import img4 from "../images/Amazon.png";
+import img5 from "../images/sidcupgolf.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowTrendDown } from "@fortawesome/free-solid-svg-icons";
+
 const Projects = () => {
   return (
     <div className="h-920px md:h-[920px] bg-gray-700 md:pl-20">
@@ -10,12 +18,43 @@ const Projects = () => {
         </h3>
       </div>
 
-      <div className="flex justify-center items-center bg-white md:h-[550px] md:w-[1350px] mt-8 rounded-xl shadow-2xl">
-        <div>
-            <img src="https://imgv3.fotor.com/images/blog-cover-image/part-blurry-image.jpg" alt="" />
+      <div className="flex justify-center items-center bg-gray-500 md:h-[550px] h-[300px] md:mt-8 ml-8 md:mr-20 mr-8 rounded-3xl shadow-2xl ">
+        <div className="">
+          <div className="md:w-full flex justify-center items-center flex-wrap gap-3 flex-row  md:gap-6">
+            <img
+              className=" h-[30px] rounded-lg md:mt-6 mt-2  md:h-[180px]"
+              src={img4}
+              alt=""
+            />
+            <img
+              className=" h-[30px] rounded-lg md:mt-6 mt-2  md:h-[180px]"
+              src={img5}
+              alt=""
+            />
+            <img
+              className="  h-[30px] rounded-lg md:mt-6 mt-2  md:h-[180px]"
+              src={img1}
+              alt=""
+            />
+            <img
+              className=" h-[30px] rounded-lg md:mt-6 mt-2  md:h-[180px]"
+              src={img2}
+              alt=""
+            />
+            <img
+              className=" h-[30px] rounded-lg md:mt-6 mt-2  md:h-[180px]"
+              src={img3}
+              alt=""
+            />
+          </div>
         </div>
+      </div>
 
-
+      <div className="text-white  flex justify-center items-center gap-8 ">
+        <FontAwesomeIcon className="text-7xl" icon={faArrowTrendDown} />
+        <div className="">
+          <h3 className="mt-[90px] font-bold text-2xl bg-black ">See All my work</h3>
+        </div>
       </div>
     </div>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,18 @@
 import Designing from "./Designing";
+import Hobbies from "./Hobbies";
 import TechStack from "./TechStack";
-import videoimage from "../images/videography.JPG";
 
 const Skills = () => {
   return (
-    <div className="md:h-[920px] bg-gray-800 md:pl-20">
+    <div className=" md:h-[1000px] h-[500px] bg-gray-800 md:pl-20">
       <div className="font-bold text-white  text-4xl  md:text-7xl pt-8">
         What I'm <span className="text-red-500 font-bold "> Skilled </span>In
         <h3 className=" text-white text-xl md:text-3xl md:pl-2">
-          interests, passion and hobbies 👨‍💻🏏🎥 
+          interests, passion and hobbies 👨‍💻🏏🎥
         </h3>
       </div>
       <div>
-        <div className="w-[100px] md:w-[210px] text-center md:h-12  text-sm md:text-3xl font-medium bg-gray-800 border-2 border-white text-white rounded-xl md:mt-20 mt-6">
+        <div className="w-[100px] md:w-[210px] text-center md:h-12  text-sm md:text-3xl font-medium bg-gray-800 border-2 border-white text-white rounded-xl md:mt-14 mt-4 ">
           development
         </div>
       </div>
@@ -20,38 +20,25 @@ const Skills = () => {
         <TechStack />
       </div>
       <div>
-        <div className="w-[70px] md:w-[160px] text-center md:h-12  text-sm md:text-3xl font-medium bg-gray-800 border-2 border-white text-white rounded-xl md:mt-20 mt-10">
+        <div className="w-[70px] md:w-[160px] text-center md:h-12  text-sm md:text-3xl font-medium bg-gray-800 border-2 border-white text-white rounded-xl md:mt-20 mt-4">
           designing
         </div>
       </div>
       <div>
         <Designing />
       </div>
+
       <div>
-        <div className="w-[70px] md:w-[160px] text-center md:h-12  text-sm md:text-3xl font-medium bg-gray-800 border-2 border-white text-white rounded-xl md:mt-20 mt-10">
+        <div className="w-[70px] md:w-[160px] text-center md:h-12  text-sm md:text-3xl font-medium bg-gray-800 border-2 border-white text-white rounded-xl md:mt-20 mt-4">
           hobbies
         </div>
       </div>
-      <div className="md:w-full flex flex-wrap gap-3 flex-row  md:gap-6">
-        <img
-          className="h-[40px] rounded-lg mt-6  md:h-[80px]"
-          src={videoimage}
-          alt=""
-        />
-
-        <img
-          className="h-[40px] rounded-lg mt-6  md:h-[80px]"
-          src="https://imagevars.gulfnews.com/2023/03/13/Virat-Kohli-_186db67304d_medium.jpg"
-          alt=""
-        />
-
-        <img
-          className="h-[40px] rounded-lg mt-6  md:h-[80px]"
-          src="https://1.bp.blogspot.com/-DSs71-t6-HI/UoxY0j8IF6I/AAAAAAAAEgQ/IcgJax9Im5w/s1600/vlcsnap-2013-11-17-23h50m01s172.jpg"
-          alt=""
-        />
+      <div>
+        <Hobbies />
       </div>
-    </div>
+
+      </div>
+
   );
 };
 
