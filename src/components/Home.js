7728f7d90fd6b501de63ecd0ce12ca116dd9f698
imdeepakvermaa <dv24012002@gmail.com>
2023-11-11 23:25:46 +0530

    Final

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my-profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full "
+            className="rounded-2xl mx-auto w-2/3 md:2/3 "
           />
         </div>
       </div>
