2b285eb40bee75619aa9e55408931d2c5b6980a2
imdeepakvermaa <dv24012002@gmail.com>
2024-07-17 16:59:47 +0530

    ss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
+import HeroImage from "../images/HeroImage.png";
+
 const Home = () => {
   return (
     <div
@@ -10,12 +11,16 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-8  md:flex-row">
         <div className="flex flex-col justify-center h-full gap-4 mt-40 ">
           <p className="text-gray-400  max-w-md font-bold text-xl">
-          Heya, I'm Deepak Verma - a data science Enthuthiast from India.
-Data nerd. Learning. Creativity.
-      
+            Heya, I'm Deepak Verma - a data science Enthuthiast from India. Data
+            nerd. Learning. Creativity.
           </p>
           <div>
-            <Link to="projects" smooth duration={500} className="group w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link
+              to="projects"
+              smooth
+              duration={500}
+              className="group w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Projects
               <span className="group-hover:rotate-90 duration-300 ">
                 <MdOutlineKeyboardArrowRight size={25} />
@@ -25,7 +30,7 @@ Data nerd. Learning. Creativity.
         </div>
 
         <div className="">
-          
+          <img src={HeroImage} style={{"height":"25rem","margin":"8rem 0rem 0rem 0rem"}} alt="Alternative" />
         </div>
       </div>
     </div>
